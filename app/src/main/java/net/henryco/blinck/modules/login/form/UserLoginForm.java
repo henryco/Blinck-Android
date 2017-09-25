defece544431c72844427d6687ecbf595cca55dc
HenryCo <hd2files@gmail.com>
2017-09-25 04:48:25 +0200

    Facebook login activity todo: FacebookLoginBroker tests

--- app/src/main/java/net/henryco/blinck/modules/login/form/UserLoginForm.java
+++ app/src/main/java/net/henryco/blinck/modules/login/form/UserLoginForm.java
@@ -1,8 +1,18 @@
 package net.henryco.blinck.modules.login.form;
 
+import com.google.gson.annotations.SerializedName;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 /**
  * Created by HenryCo on 25/09/17.
  */
+@Data @NoArgsConstructor
+@AllArgsConstructor
+public final class UserLoginForm {
 
-public class UserLoginForm {
+	private @SerializedName("facebook_uid") String uid;
+	private @SerializedName("facebook_token") String token;
+
 }
