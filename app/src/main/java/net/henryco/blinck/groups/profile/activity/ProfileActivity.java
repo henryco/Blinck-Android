1303a9333615c7185629be102da88b2e901db529
HenryCo <hd2files@gmail.com>
2017-09-29 21:02:47 +0200

    Facebook login activity todo: FacebookLoginBroker tests

--- app/src/main/java/net/henryco/blinck/groups/profile/activity/ProfileActivity.java
+++ app/src/main/java/net/henryco/blinck/groups/profile/activity/ProfileActivity.java
@@ -1,10 +1,13 @@
 package net.henryco.blinck.groups.profile.activity;
 
 import android.os.Bundle;
+import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.view.View;
 import net.henryco.blinck.R;
 import net.henryco.blinck.util.reflect.AutoFind;
 import net.henryco.blinck.util.reflect.AutoFinder;
@@ -16,6 +19,13 @@ public class ProfileActivity extends AppCompatActivity {
 	@AutoFind(R.id.toolbar)
 	private Toolbar toolbar;
 
+	@AutoFind(R.id.fab)
+	private FloatingActionButton fab;
+
+	@AutoFind(R.id.toolbar_layout)
+	private CollapsingToolbarLayout collapsingToolbarLayout;
+
+
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -26,19 +36,31 @@ public class ProfileActivity extends AppCompatActivity {
 
 		setSupportActionBar(toolbar);
 
-		FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
 		fab.setOnClickListener(view ->
 				Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
 						.setAction("Action", null).show()
 		);
 
+		getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
+		collapsingToolbarLayout.setTitle("someTitle");
 	}
 
 
 	@Override
-	public void onBackPressed() {
+	public boolean onOptionsItemSelected(MenuItem item) {
 
+		switch (item.getItemId()) {
+			case android.R.id.home:
+				onBackPressed();
+				break;
+		}
+
+		return super.onOptionsItemSelected(item);
+	}
+
+	@Override
+	public void onBackPressed() {
 		setResult(RESULT_OK);
 		finish();
 	}
